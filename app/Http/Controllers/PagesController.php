480c9daee0534eec31c027d0dbd112c33ca138e4
DeanoPHP <deanolark@gmail.com>
2020-12-28 16:36:31 +0000

    updating about page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -22,8 +22,8 @@ class PagesController extends Controller
             'image' => 'logo.png',
             'whoWeAre' => '',
             'whatMatters' => '',
-            'history' => 'Hi my name is Dean Lark and I\'m the owner of DLcleaners. Currently it is just myself working for the company but I am looking to expand. I started DLcleaners in 2018 and its been a success from the get go. I started by doing just domestic cleaning but soon moved on to cleaning for the BRI hospital, having contracts with Yatton Fish Bar, Yatton Dental and Implant Center. I have also been working for number of estate agents.',
-            'historyP2' => 'Before I went travelling in 2006 I worked for a cleaning company that done alot of work for the council. It really was a job I found satisfaction in if you can believe that. Its a job where you can see the results of hards day work which is very rewarding, but I think the most rewarding thing about being a cleaner is seeing the look in peoples faces after i have completely changed there household, office, shop etc etc.'
+            'history' => 'Hi my name is Dean Lark and I\'m the owner of DLcleaners. Currently it is just myself working for the company but I am looking to expand. I started DLcleaners in 2018 and its been a success from the get go. I started by doing just domestic cleaning but soon moved on to cleaning for the BRI hospital, having contracts with Yatton Fish Bar, Yatton Dental and Implant Center and a number of estate agents.',
+            'historyP2' => 'Before I went travelling in 2006 I worked for a cleaning company that done alot of work for the council. It really was a job I found satisfaction in if you can believe that. Its a job where you can see the results of a hards day work which I find very rewarding, but I think the most rewarding thing about being a cleaner is seeing the look in peoples faces after they see the results of my work!!'
         );
 
         return view('pages.about')->with($data);
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     
     <div class="jumbotron">
-        <img src="images/{{ $image }}" width="100%" height="200px" alt="">
+        <img src="images/{{ $image }}" width="100%" height="200px" alt="DLcleaners logo">
     </div>
     
     <h1 style="font-family: Times New Roman;font-size: 60px;text-align:center;">History</h1>
@@ -16,4 +16,21 @@
             <p style="font-size:20px;">{{ $historyP2 }}</p>
         </div>
     </div>
+    <hr>
+    <div class="row">
+        
+        <div class="col-md-4">
+            <img src="images/IMG_6960.jpeg" width="100%" height="200px" alt="Hospital Cleaner with dlcleaners">
+        </div>
+        
+        <div class="col-md-4">
+            <img src="images/IMG_6821.jpeg" width="100%" height="200px" alt="cleaning surgeries with dlcleaners">
+        </div>
+        
+        <div class="col-md-4">
+            <img src="images/IMG_6844.jpeg" width="100%" height="200px" alt="Clean sink from dlcleaners">
+        </div>
+
+    </div>
+    <hr>
 @endsection
