2ef349c136d51831d9583226912fbe2eff89d949
DeanoPHP <deanolark@gmail.com>
2020-12-28 16:07:42 +0000

    update to about us page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+    
     <div class="jumbotron">
-        <img src="storage/cover_image/{{ $image }}" width="100%" height="200px" alt="">
+        <img src="images/{{ $image }}" width="100%" height="200px" alt="">
     </div>
-    <h1>{{ $title }}</h1>
-    <p>{{ $sub }}</p>
+    
+    <h1 style="font-family: Times New Roman;font-size: 60px;text-align:center;">History</h1>
+
+    <div class="row">
+        <div class="col-md-6">
+            <p style="font-size:20px;">{{ $history }}</p>
+        </div>
+        <div class="col-md-6">
+            <p style="font-size:20px;">{{ $historyP2 }}</p>
+        </div>
+    </div>
 @endsection
