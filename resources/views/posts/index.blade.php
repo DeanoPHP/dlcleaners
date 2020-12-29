a8d219c27daceb049f1d7f190c0774f0ca22b57c
DeanoPHP <deanolark@gmail.com>
2020-12-29 12:16:38 +0000

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
             <div class="card m-3 p-3">
                 <div class="row">
                     <div class="col-md-4 col-sm-4">
-                        <img style="width:100%;height:300px;" src="storage/cover_image/{{$post->cover_image}}" alt="Post Image">
+                        <img style="width:100%;height:300px;margin-bottom:20px;" src="storage/cover_image/{{$post->cover_image}}" alt="Post Image">
                     </div>
                     <div class="col-md-8 col-sm-8">
                         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
