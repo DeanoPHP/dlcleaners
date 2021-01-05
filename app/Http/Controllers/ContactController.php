6ab6c2d8184f08dab0736a47650f0307602fe19c
DeanoPHP <deanolark@gmail.com>
2021-01-05 22:42:12 +0000

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -23,6 +23,6 @@ class ContactController extends Controller
         });
 
         echo "<script>alert('Your message has been sent successfully and we get back to you within 24 hours')</script>";
-        return redirect('/');
+        return redirect('/contact');
     }
 }
